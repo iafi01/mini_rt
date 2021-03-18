ca69564dcf9db45313ce208f5966492ae4aecff0
Leonardo Mizzoni <lmizzoni@e3r6p7.42roma.it>
2021-03-18 15:00:19 +0100

    Aggiunti 3 file contenenti le operazioni tra vettori e tra punti-vettori

---
+++ files/point_op.c
@@ -0,0 +1,29 @@
+#include "minirt.h"
+
+// Somma tra un punto e un vettore
+
+t_point			sum_p_vec(t_point p, t_vector v)
+{
+	return(create_p(p.x + v.x, p.y + v.y, p.z + v.z));
+}
+
+// Differenza tra un punto e un vettore
+
+t_point			diff_p_vec(t_point p, t_vector v)
+{
+	return(create_p(p.x - v.x, p.y - v.y, p.z - v.z));
+}
+
+// Moltiplicazione di un punto per uno scalare
+
+t_point			mult_point_scal(t_point p, float a)
+{
+	return(create_p(p.x * a, p.y * a, p.z * a));
+}
+
+// Negare un punto
+
+t_point			neg_point(t_point p)
+{
+	return(mult_point_scal(p, (-1.0)));
+}
---
+++ files/vec_point_base.c
@@ -0,0 +1,21 @@
+#include "minirt.h"
+
+t_point		create_p(float x, float y, float z)
+{
+	t_point tmp;
+
+	tmp.x = x;
+	tmp.y = y;
+	tmp.z = z;
+	return (tmp);
+}
+
+t_vector	create_v(float x, float y, float z)
+{
+	t_vector tmp;
+
+	tmp.x = x;
+	tmp.y = y;
+	tmp.z = z;
+	return (tmp);
+}
