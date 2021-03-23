cb934c8006df2857ccd9f2c46a8f411701850558
IAFI <67833440+iafi01@users.noreply.github.com>
2021-03-23 11:52:32 +0100

    Add files via upload

---
+++ files/elements.h
@@ -0,0 +1,158 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   elements.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: liafigli <liafigli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/15 16:57:32 by liafigli          #+#    #+#             */
+/*   Updated: 2021/03/23 11:50:14 by liafigli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ELEMENTS
+# define ELEMENTS
+
+typedef struct s_point
+{
+    float x;
+    float y;
+    float z;
+}t_point;
+
+typedef struct s_vector
+{
+    float x;
+    float y;
+    float z;
+}t_vector;
+
+typedef struct s_color
+{
+    float r;
+    float g;
+    float b;
+}t_color;
+
+//MLX_RESOURCES/COLORS/PARSING/RESOLUTION/AMBIENT/CAMERA/LIGHT
+
+typedef struct s_global{
+    void       *mlx;
+    void       *mlx_win;
+
+    t_color    color1;
+    t_color    color2;
+    float      t;
+    t_point    point;
+
+    char       *line;
+    char       **split;
+    int        fd;
+    
+    int        width;
+    int        height;
+
+    float      range;
+    t_color    color;
+
+}t_global;
+
+typedef struct s_viewport
+{
+	t_point lower_left_corner;
+	t_vector hor_axis;
+	t_vector ver_axis;
+	t_point origin;
+}				t_viewport;
+
+typedef struct  s_imgdata {
+    void        *img;
+    char        *addr;
+    int         bits_per_pixel;
+    int         line_length;
+    int         endian;
+}               t_imgdata;
+
+typedef struct s_ray
+{
+    t_point origin;
+    t_vector dir;
+}t_ray;
+
+typedef struct s_calc
+{
+	float a;
+	float b;
+	float c;
+	float delta;
+	float t1;
+	float t2;
+	t_point sub;
+}t_calc;
+
+typedef struct s_resolution
+{
+    int x;
+    int y;
+}t_resolution;
+
+typedef struct s_ambient
+{
+    float range;
+    t_color color;
+}t_ambient;
+
+typedef struct s_camera
+{
+    t_point position;
+    t_vector direction;
+    int fov;
+}t_camera;
+
+typedef struct s_light
+{
+    t_point origin;
+    float   range;
+    t_color color;
+}t_light;
+
+typedef struct s_sphere
+{
+    t_point origin;
+    float ray;
+    t_vector color;
+}t_sphere;
+
+typedef struct s_plane
+{
+    t_point center;
+    t_vector orientation; //-1 to 1
+    t_color color;
+}t_plane;
+
+typedef struct s_square
+{
+    t_point center;
+    float range;
+    float side;
+    t_color color;
+}t_square;
+
+typedef struct s_cylinder
+{
+    t_point center;
+    float range;
+    float diameter;
+    float height;
+    t_color color;
+}t_cylinder;
+
+typedef struct s_triangle
+{
+    t_point p1;
+    t_point p2;
+    t_point p3;
+    t_color color;
+}t_triangle;
+
+#endif
--- files/parse.c
+++ files/parse.c
@@ -6,7 +6,7 @@
 /*   By: liafigli <liafigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 12:40:18 by liafigli          #+#    #+#             */
-/*   Updated: 2021/03/22 19:34:05 by liafigli         ###   ########.fr       */
+/*   Updated: 2021/03/23 11:51:39 by liafigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,37 +63,63 @@ int parse_resolution(t_global *a, char **words)
         a->height = MAX_HEIGHT;
     return (0);
 }
-///cleean to make
+///si moltiplicherá per la luce, ovvero luce ambientale per luce normale
 int parse_ambient(t_global *a, char **words)
-{   words = 0;
-    a->t = 1;
-
-    //qualcosa aborta il mondoooo
-    /*char **split;
+{   
+    char **split;
     t_color col;
     
     a->range = ft_atof(words[1]);
     split = ft_split(words[2], ',');
-    col.r = ft_atof(split[0]) / 255;
-	col.g = ft_atof(split[1]) / 255;
-	col.b = ft_atof(split[2]) / 255;
+    
+    col.r = atof(split[0]) / 255;
+	col.g = atof(split[1]) / 255;
+	col.b = atof(split[2]) / 255;
     a->color = col;
+    
     if (a->range > 1 || a->range < 0)
-        ft_putstr("Error ambient in rt file");*/
+        ft_putstr("Error ambient in rt file");
     return (0);
 }
 
 int parse_light(t_global *a, char **words)
 {
-        words = 0;
+    char **origin;
+    char **color;
+    t_color col;
+    t_light light;
 
-    a->t = 1;
+    origin = ft_split(words[1], ',');
+    light.origin.x = ft_atoi(origin[0]);
+    light.origin.y = ft_atoi(origin[1]);
+    light.origin.z = ft_atoi(origin[2]);
+    light.range = ft_atof(words[1]);
+
+    color = ft_split(words[2], ',');
+    
+    col.r = atof(color[0]) / 255;
+	col.g = atof(color[1]) / 255;
+	col.b = atof(color[2]) / 255;
+    light.color = col;
     return (0);
 }
 
 int parse_camera(t_global *a, char **words)
 {
-    words = 0;
-    a->t = 1;
+    t_camera camera;
+    char **position;
+    char **direction;
+
+    position = ft_split(words[1], ',');
+    camera.position.x = ft_atoi(position[0]);
+    camera.position.y = ft_atoi(position[1]);
+    camera.position.z = ft_atoi(position[2]);
+
+    direction = ft_split(words[2], ',');
+    camera.direction.x = ft_atoi(direction[0]);
+    camera.direction.y = ft_atoi(direction[1]);
+    camera.direction.z = ft_atoi(direction[2]);
+
+    camera.fov = ft_atoi(words[3]);
     return (0);
 }
